6d2cee8ea9fb9685f470a5cf2a5786eb5d803811
Gabriel Mattos <103071409+mattosbe@users.noreply.github.com>
2022-12-17 18:49:27 -0300

    Delete 23.py

--- 100days/day2.py/23.py
+++
@@ -1,5 +0,0 @@
-age = int(input("What is your current age? "))
-age_days = (age - 90) * 365
-age_weeks = (age - 90) * 52
-age_months = (age - 90) * 12
-print(f"You have {abs(age_days)} days, {abs(age_weeks)} weeks, and {abs(age_months)} months left.")
